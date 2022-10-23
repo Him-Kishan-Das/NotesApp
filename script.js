e129ce290cd01a3a450cf17f7612f5e6a84b0010
Him-Kishan_Das <himkishandas456@gmail.com>
2022-10-23 23:03:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+function save(){
+    var key = document.getElementById('note-title').value;
+    let a = document.getElementById("note-field").value;
+    localStorage.setItem(key, a);
+    
+    let html = '';
+for (let [a, b] of Object.entries(localStorage)) {
+    
+    html += `<p class="display">${a} <br> ${b} </p>`;
+   
+  }
+  document.getElementById('note-display').innerHTML = html;
+
+    document.getElementById('note-title').value = '';
+    document.getElementById("note-field").value = '';
+}
+
+let html = '';
+for (let [a, b] of Object.entries(localStorage)) {
+    
+    html += `<p class="display_key">${a} </p> <p class="display_value"> ${b} </p>`;
+   
+  }
+  document.getElementById('note-display').innerHTML = html;
